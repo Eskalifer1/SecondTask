a2b2fd40fb113a6ab4369fc0e4b9ce153b489954
Artem Achkasov <artemkryt1800@gmail.com>
2022-11-12 17:55:04 +0200

    images was minified

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+// Swipers
 const swiperCompetitions = new Swiper('.swiper',{
 
     //Touching
@@ -61,3 +62,53 @@ const swiperRespond = new Swiper(".swiper-respond", {
         clickable: true
     },
 });
+
+const lazyImages = document.querySelectorAll('img[data-src]');
+const windowHeight = document.documentElement.clientHeight;
+
+let lazyImagePositions = [];
+
+if(lazyImages.length > 0){
+    lazyImages.forEach(img => {
+        if(img.dataset.src){
+            lazyImagePositions.push(img.getBoundingClientRect().top + window.pageYOffset);
+            lazyScrollCheck();
+        }
+    });
+}
+
+window.addEventListener("scroll",lazyScroll, { capture: true });
+
+function lazyScroll(){
+    if(document.querySelectorAll('img[data-src]').length > 0){
+        lazyScrollCheck();
+    }
+}
+
+function lazyScrollCheck(){
+    let imgIndex = lazyImagePositions.findIndex(
+        item => (self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop)) >= (item - windowHeight)
+    );
+    if(imgIndex >= 0){
+        if(lazyImages[imgIndex].dataset.src){
+            lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
+            lazyImages[imgIndex].removeAttribute('data-src');
+            setTimeout(()=>{
+                updateDuplicateSlides();
+            })
+        }
+        delete lazyImagePositions[imgIndex];
+    }
+}
+
+updateDuplicateSlides = function() {
+    var swiper = this;
+    var $wrapperEl = swiper.$wrapperEl;
+    var params = swiper.params;
+    var slides = swiper.slides;
+    $wrapperEl.children(("." + (params.slideClass) + "." + (params.slideDuplicateClass)))
+    .each(function() {
+      var idx = this.getAttribute('data-swiper-slide-index');
+      this.innerHTML = $wrapperEl.children('.'+params.slideClass+'[data-swiper-slide-index="'+idx+'"]:not(.'+params.slideDuplicateClass+')').html();
+  })
+}
