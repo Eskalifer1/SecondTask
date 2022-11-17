37626d8f7ad130464869b457a8b703369e7e72ff
Artem Achkasov <artemkryt1800@gmail.com>
2022-11-17 18:35:51 +0200

    Js was fixed && added preload

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,14 @@
-// Swipers
 const swiperCompetitions = new Swiper('.swiper',{
-
-    //Touching
     simulateTouch:true,
     touchRatio: 0.25,
     grabCursor: true,
-
-    //Keyboard controll
     keyboard: {
         enabled: true,
         onlyInViewport: true,
         pageUpDown: true
     },
-    
     slidesPerView: 4,
-
     spaceBetween: 30,
-
     slidesPerGroup: 1,
 
     loop: true,
@@ -32,7 +24,6 @@ const swiperCompetitions = new Swiper('.swiper',{
     observeSlideChildren: true,
 
     followFinger: true,
-
     breakpoints:{
         1025:{
             slidesPerView: 4
@@ -66,26 +57,18 @@ const swiperCompetitions = new Swiper('.swiper',{
         }
     }
 });
-
 const swiperRespond = new Swiper(".swiper-respond", {
-    //Touching
     simulateTouch:false,
     touchRatio: 0.25,
     speed:800,
-
-    //Keyboard controll
     keyboard: {
         enabled: true,
         onlyInViewport: true,
         pageUpDown: true
     },
-    
     slidesPerView: 1,
-
     spaceBetween: 100,
-
     slidesPerGroup: 1,
-
     pagination: {
         el: '.swiper-pagination',
         clickable: true
@@ -99,21 +82,19 @@ const swiperRespond = new Swiper(".swiper-respond", {
 
 const lazyImages = document.querySelectorAll('img[data-src]');
 let windowHeight = document.documentElement.clientHeight;
-
+let burger = document.querySelector(".header__burger");
+let nav = document.querySelector(".nav");
+let body = document.querySelector("body");
 function orientationChecker(){
     windowHeight = document.documentElement.clientHeight;
     windowWidth = document.documentElement.clientWidth;
-
     if(windowWidth > (windowHeight - 60)){
         document.querySelector("body").classList.add("resize");
     } else document.querySelector("body").classList.remove("resize");
 }
-
-// Resize
 window.addEventListener("load", orientationChecker)
 window.addEventListener('resize',orientationChecker);
 let lazyImagePositions = [];
-
 if(lazyImages.length > 0){
     lazyImages.forEach(img => {
         if(img.dataset.src){
@@ -122,15 +103,12 @@ if(lazyImages.length > 0){
         }
     });
 }
-
 window.addEventListener("scroll",lazyScroll, { capture: true });
-
 function lazyScroll(){
     if(document.querySelectorAll('img[data-src]').length > 0){
         lazyScrollCheck();
     }
 }
-
 function lazyScrollCheck(){
     let imgIndex = lazyImagePositions.findIndex(
         item => (self.pageYOffset || (document.documentElement && document.documentElement.scrollTop) || (document.body && document.body.scrollTop)) >= (item - windowHeight)
@@ -139,31 +117,10 @@ function lazyScrollCheck(){
         if(lazyImages[imgIndex].dataset.src){
             lazyImages[imgIndex].src = lazyImages[imgIndex].dataset.src;
             lazyImages[imgIndex].removeAttribute('data-src');
-            setTimeout(()=>{
-                // updateDuplicateSlides();
-                update();
-            })
         }
         delete lazyImagePositions[imgIndex];
     }
 }
-
-// updateDuplicateSlides = function() {
-//     var swiper = this;
-//     var $wrapperEl = swiper.$wrapperEl;
-//     var params = swiper.params;
-//     var slides = swiper.slides;
-//     $wrapperEl.children(("." + (params.slideClass) + "." + (params.slideDuplicateClass)))
-//     .each(function() {
-//       var idx = this.getAttribute('data-swiper-slide-index');
-//       this.innerHTML = $wrapperEl.children('.'+params.slideClass+'[data-swiper-slide-index="'+idx+'"]:not(.'+params.slideDuplicateClass+')').html();
-//   })
-// }
-
-let burger = document.querySelector(".header__burger");
-let nav = document.querySelector(".nav");
-let body = document.querySelector("body");
-
 burger.addEventListener("click", function(){        
     nav.classList.toggle("active");
     burger.classList.toggle("active");
